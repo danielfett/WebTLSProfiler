adc6820dce8ba56f927e69f8e5e043ef5fc2fa83
Daniel Fett <danielf@yes.com>
2020-04-02 20:12:03 +0200

    Use autoscale for celery to reduce memory consumption.

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 python3 manage.py migrate &
 redis-server &
-celery -A wwwtlsprofiler worker --concurrency=10 -l info &
+celery -A wwwtlsprofiler worker --autoscale=10,0 -l info &
 gunicorn -b 0.0.0.0:8000 --access-logfile - wwwtlsprofiler.wsgi
